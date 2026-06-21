547fef7f97911bc3fe0ad2a6f1afba75d4a02699
radhika-khatri <radhika-khatri@users.noreply.github.com>
2026-06-21 13:42:13 +0400

    Commented unused variables

--- src/guards/PublicOnlyRoute.tsx
+++ src/guards/PublicOnlyRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 
 import { useAuth } from "../hooks/useAuth";
 
@@ -10,7 +10,7 @@ export default function PublicOnlyRoute({
   children,
 }: Props) {
   const {
-    user,
+    // user,
     loading,
   } = useAuth();
 
